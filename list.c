a89d435b0cf83fbac2f383a56cd5b07da65a896d
Anthony Bedford <tony.bedford@live.co.uk>
2017-09-21 17:46:55 +0100

    Added list functions.

--- list.c
+++ list.c
@@ -1,33 +1,125 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// test a list
+// !! No error checking !!
+
+typedef unsigned int data_t;
+
+typedef enum { false, true } bool;
 
 typedef struct node_s {
 
-    node_s *next;
-    node_s *prev;
-    unsigned int data;
+    struct node_s *next;
+    struct node_s *prev;
+    data_t data;
     
 } node_t;
 
 typedef struct list_s {
     node_t *head;
     node_t *tail;
-    unsigned int items; // number of nodes in list
-}
+    size_t items; // number of nodes in list
+} list_t;
 
 // create list
 
+void create_list (list_t *list)
+{
+    list->head = NULL;
+    list->tail = NULL;
+    list->items = 0;
+}
+
+bool empty_list (list_t *list)
+{
+    if (list->head == NULL)
+        return true;
+    return false;
+}
+    
 // create node
 
-// add node (at head)
+node_t *create_node (data_t data)
+{
+    node_t *n = (node_t *) malloc (sizeof(node_t));
+    n->next = NULL;
+    n->prev = NULL;
+    n->data = data;
 
-// add node (at tail)
+    return n;
+}
 
-// insert node
+// add node n to end of list l 
+void add_node (list_t *l, node_t *n)
+{
+    if (l->items == 0) // empty list
+    {
+        l->head = n;
+        l->tail = n;
+        l->items = 1;
+    }
+    else // add after tail
+    {
+        n->prev = l->tail;
+        n->next = NULL;
+        l->tail->next = n;
+        l->tail = n;
+    }
+    
+}
 
-// remove node
+// insert node n after node p
+
+void insert_node (list_t *list, node_t *n, node_t *p)
+{
+        node_t *temp = p->next;
+        temp->prev = p;
+        p->next = n;
+        n->next = temp;
+        n->prev = p;
+}
+
+// remove node (from point p)
     
 // print list
+
+void print_list (list_t *l)
+{
+    node_t *rover = l->head;
+
+    do {
+        printf ("Data: %d\n", (unsigned int)rover->data);
+        rover = rover->next;
+    }
+    while (rover != NULL);
+}
+
+// destroy node
+
+// destroy list
+
+// main
+
+int main (int c, char **argv)
+{
+
+    list_t list;
+    node_t *node;
+    
+    create_list (&list);
+
+    node = create_node (12);
+    add_node(&list, node);
+
+    node = create_node (34);
+    add_node(&list, node);
+
+    node = create_node (56);
+    add_node(&list, node);
+
+    print_list(&list);
+
+    return 0;
+}
 
     
