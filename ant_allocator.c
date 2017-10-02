bc207691621b68cfea3359c6cf718df3668f79f7
Anthony Bedford <tony.bedford@live.co.uk>
2017-10-02 14:00:37 +0100

    Added tests.

--- ant_allocator.c
+++ ant_allocator.c
@@ -332,30 +332,35 @@ int main (int argc, char **argv)
     printf ("FILE:%s LINE:%d\n", __FILE__, __LINE__);
     
     BLOCK_HDR_SZ = sizeof(block_hdr_t);
-    allocator_t allocator;
+    allocator_t alloc;
     size_t heap_sz = 16000;
 
-    allocator_create(&allocator, heap_sz);
+    allocator_create(&alloc, heap_sz);
 
-    void *p1 = ant_alloc(&allocator, 2000);
-    void *p2 = ant_alloc(&allocator, 1000);
-    void *p3 = ant_alloc(&allocator, 3000);
+    void *p1 = ant_alloc(&alloc, 2000);
+    void *p2 = ant_alloc(&alloc, 1000);
+    void *p3 = ant_alloc(&alloc, 3000);
     
-    ant_free (&allocator, p2);
+    ant_free (&alloc, p2);
 
-    void *p4 = ant_alloc(&allocator, 500);
+    void *p4 = ant_alloc(&alloc, 500);
 
-    ant_free(&allocator, p3);
+    ant_free(&alloc, p3);
 
-    void *p5 = ant_alloc(&allocator, 1500);
+    void *p5 = ant_alloc(&alloc, 1500);
 
-    ant_free(&allocator, p1);
+    ant_free(&alloc, p1);
 
-    void *p6 = ant_alloc(&allocator, 1000);
+    void *p6 = ant_alloc(&alloc, 1000);
+
+    dump_heap(&alloc);
     
-    dump_heap(&allocator);
-    check_heap(&allocator);
-    allocator_destroy (&allocator);
+    void *p7 = ant_alloc(&alloc, 4000);
+    ant_free (&alloc, p7);
+    
+    dump_heap(&alloc);
+    check_heap(&alloc);
+    allocator_destroy (&alloc);
 
     return 0;
 }
