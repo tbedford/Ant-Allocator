41715b839cd16a72973ab633452372ca96cef405
Anthony Bedford <tony.bedford@live.co.uk>
2017-10-14 09:46:03 +0100

    Added new test.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "ant_allocator.h"
 
-int main (int argc, char **argv)
+void test1 ()
 {
     memobj_t memobj[8];
     
@@ -26,8 +26,40 @@ int main (int argc, char **argv)
     }
 
     heap_dump();
-    
     heap_destroy();
+}
 
+void test2 ()
+{
+    size_t heap_sz = 9000;
+    size_t request_sz = 1000;
+
+    heap_create(heap_sz);
+
+    void *p1 = ant_alloc(request_sz);
+    void *p2 = ant_alloc(request_sz);
+    void *p3 = ant_alloc(request_sz);
+    void *p4 = ant_alloc(request_sz);
+    
+    heap_dump();
+
+    ant_free(p2, request_sz);
+    ant_free(p4, request_sz);
+    
+    heap_dump();
+
+    ant_free(p1, request_sz);
+    ant_free(p3, request_sz);
+
+    heap_dump();
+
+    heap_destroy();
+}
+
+int main (int argc, char **argv)
+{
+//    test1();
+    test2();
+    
     return 0;
 }
